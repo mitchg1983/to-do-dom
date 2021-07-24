6717990cc38a254c16af6c17dc1e518850276f85
mitchg1983 <mitchg83@Geoffreys-MacBook-Pro.local>
2021-07-24 17:35:02 -0400

    Improve code

--- main.js
+++ main.js
@@ -1,41 +1,41 @@
+//Establish DOM elements
+
+const displayArea = $("ul.displayList");
+
+
+//tasks is an array of objects, where each object is a task on the person's list.
 let tasks = [
-  
-    {
-      taskName: "Eat a bagel.",
-      priority: 0,
-      dueDate: 0,
-      comments: "",
-    }
-  ,
-  
-    {
-      taskName: "Drink some coffee.",
-      priority: 0,
-      dueDate: 0,
-      comments: "",
-    },
-  
+  {
+    taskName: "Eat a bagel.",
+    priority: 0,
+    dueDate: 0,
+    comments: "",
+  },
+  {
+    taskName: "Drink some coffee.",
+    priority: 0,
+    dueDate: 0,
+    comments: "",
+  },
 ];
 
-let taskList = $("ul.displayList");
-
-$.each(tasks, function(index, value) {
 
 
 
-const outputTask = value.taskName;
-
-const displayArea = $('ul.displayList');
-
-const newListItem = $('<li class="task"></li>');
 
 
 
-$(newListItem).text(outputTask);
-console.log (`${$(newListItem).text()}`);
+//for each object in the tasks array, add a corresponding list item for the user to see
+$.each(tasks, function (index, value) {
 
+  const newListItem = $('<li class="task"></li>');
 
-$(displayArea).append(newListItem);
+  //Change the text value of the new list item to be the task. Might need to add in
+  //a check to the length of the string. So a person won't need to see a whole paragraph
+  //printed out.
+  $(newListItem).text(value.taskName);
 
-    });
+  console.log(`${$(newListItem).text()}`);
 
+  $(displayArea).append(newListItem);
+});
