1923516bff3c8d153289df67f6dcb4a03ead9629
mitchg1983 <mitchg83@Geoffreys-MacBook-Pro.local>
2021-07-24 17:26:29 -0400

    Get everything talking to each other

--- main.js
+++ main.js
@@ -1,2 +1,41 @@
-console.log('Hello World!');
+let tasks = [
+  
+    {
+      taskName: "Eat a bagel.",
+      priority: 0,
+      dueDate: 0,
+      comments: "",
+    }
+  ,
+  
+    {
+      taskName: "Drink some coffee.",
+      priority: 0,
+      dueDate: 0,
+      comments: "",
+    },
+  
+];
+
+let taskList = $("ul.displayList");
+
+$.each(tasks, function(index, value) {
+
+
+
+const outputTask = value.taskName;
+
+const displayArea = $('ul.displayList');
+
+const newListItem = $('<li class="task"></li>');
+
+
+
+$(newListItem).text(outputTask);
+console.log (`${$(newListItem).text()}`);
+
+
+$(displayArea).append(newListItem);
+
+    });
 
