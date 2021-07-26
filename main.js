445b312baad5afab321c9cb2bb815a631d1c7312
mitchg1983 <mitchg83@Geoffreys-MacBook-Pro.local>
2021-07-25 21:45:19 -0400

    Finalize code

--- main.js
+++ main.js
@@ -11,12 +11,13 @@ const taskCounter = $("div.task-counter");
 //tasks is an array of objects, where each object is a task on the person's list.
 let tasks = [];
 
+let trashBin = [];
+
 let checkedT = 0;
 
-let totalT = 0;
+let totalT = -1;
 
-let currentTasks = checkedT + " / " + totalT;
-
+updateInfoBar("add");
 
 
 //
@@ -24,19 +25,14 @@ let currentTasks = checkedT + " / " + totalT;
 //
 //Add new task button
 addButton.on("click", function () {
-
-
   //Ask user for a new task
   let task = prompt("Please enter your new task...");
 
   //~task~ is just a string at this point.
   if (task !== null) {
-
-
     //Log the time this task was added
     let d = new Date();
     let currentTime = d.toUTCString();
-
 
     //This is the task object.
     const newTaskObject = {
@@ -47,21 +43,15 @@ addButton.on("click", function () {
       comments: "",
     };
 
-
     //The task object is pushed to our array for storage
     tasks.push(newTaskObject);
 
     //Next function will crete the list element to be displayed on the page
     addToList(task);
-
-
   }
 });
 
-
 function addToList(task) {
-
-
   //Make a new list element
   const newLI = $('<li class="task"></li>');
 
@@ -69,7 +59,31 @@ function addToList(task) {
   $(newLI).text(task);
 
   //Add event listener to the new list item, for when someone clicks on it.
-  newLI.on("click", clickOnTaskItem);
+  newLI.on("click", function () {
+    //task item removal function
+    if (editing === true) {
+      console.log("Editing is true.", editing);
+
+      //Save the name of the task we are deleting
+      const taskToRemove = $(newLI).text();
+
+      for (const taskObject of tasks) {
+        if (taskObject.taskName === taskToRemove) {
+          console.log("We found something.");
+
+          tasks.splice(tasks.indexOf(taskObject), 1);
+        };
+      };
+
+      $(newLI).remove();
+
+      updateInfoBar('remove');
+
+
+    } else {
+      return console.log("Editing is false.", editing);
+    };
+  });
 
   //Add this element to the 'clipboard' on the app
   $(displayArea).append(newLI);
@@ -77,47 +91,40 @@ function addToList(task) {
   //Update the display number, of total tasks on the displayed list
   //
   //Update the infobar for the user
-  totalT++;
-  updateInfoBar();
-
-};
 
 
-function clickOnTaskItem (index, value) {
-
-
-  if (editing === true) {
-
-    console.log ("Editing is true.", editing);
-
-
-
-
-
-
-
-
-
-    
-
-  } else { return console.log("Editing is false.", editing); };
-};
-
+  updateInfoBar('add');
+}
 
 //Update the display for the infobar on the app
-function updateInfoBar() {
-  //Task counter display
-  $(taskCounter).text(currentTasks);
-};
+function updateInfoBar(input) {
 
+  if (input === 'add') {
+    totalT++;
+  };
+
+  if (input === 'remove') {
+    totalT--;
+  };
+
+  if (input === 'complete') {
+    checkedT++;
+  };
+
+  //Task counter display
+  const currentTasks = checkedT + " / " + totalT;
+  $(taskCounter).text(currentTasks);
+}
 
 //Editing event listener for when the user clicks the edit button
 let editing = false;
 
 editButton.on("click", function () {
-
-  if(editing) { 
-    editing = false
-  } else { editing = true};
-
+  if (editing) {
+    $(editButton).removeClass("yes-edit").addClass("no-edit");
+    editing = false;
+  } else {
+    $(editButton).removeClass("no-edit").addClass("yes-edit");
+    editing = true;
+  }
 });
